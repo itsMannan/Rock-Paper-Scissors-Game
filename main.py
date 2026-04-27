d381c3a0d48c8113fc99d5e8ad60bf0982d8ccb6
Abdul Mannan <itz.abdulmannan123@gmail.com>
2026-04-27 06:23:28 +0500

    Implementing Dictionaries

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def get_choices():
-    person_choice = "rock"
+    person_choice = input("Enter a Choice:(rock, paper, scissor)")
     computer_choice="paper"
-    
-    return computer_choice
+    choices = {"person" : person_choice, "computer" : computer_choice}
+    return choices
 
 choices = get_choices()
 print(choices)
