561a6cc3bd3b528c94f059af8a225231e47822cf
Abdul Mannan <itz.abdulmannan123@gmail.com>
2026-05-13 10:55:56 +0500

    Refracting and Nested if-else statements

--- main.py
+++ main.py
@@ -4,19 +4,32 @@ def get_choices():
     person_choice = input("Enter a Choice:(rock, paper, scissors)")
     options = ['rock', 'paper', 'scissors']
     computer_choice = random.choice(options)
-    choices = {"person" : person_choice, "computer" : computer_choice}
+    choices = {"player" : person_choice, "computer" : computer_choice}
     return choices
 
 def check_win(player, computer):
     print(f"You chose: {player} \nComputer chose: {computer}")
     if player == computer:
         return "It's a Tie!"
-    elif player == "rock" and computer == "scissors":
-        return "Boom Player's Rock smashes Computer Scissors! You Win!"
-    elif player == "rock" and computer == "paper":
-        return "Sorry! Computer Paper covers Person's Rock! You Lose."
-
-choices = check_win('rock', 'paper')
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock Smashes Scissors! You Win!"
+        else:
+            return "Paper Covers Rock! You lose." 
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers Rock! You win!"
+        else:
+            return "Scissors cuts paper! You lose."
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! You win!"
+        else:
+            return "Rock Smashes Scissors! You lose."
+        
+choices = get_choices()
+result = check_win(choices['player'], choices['computer'])
+print(result)
 
 
 
